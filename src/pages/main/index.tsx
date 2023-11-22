01a3ca5f26ab37fb3c1e37458185ddd41361cce4
alexgm5555 <alexgm5555@hotmail.com>
2023-11-22 18:16:32 -0500

    fix: bug id country

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -9,7 +9,7 @@ export const Main:FC = () => {
   let navigate = useNavigate();
 
   const mainSuccess = (_phone: string) => {
-    const url = `https://api.whatsapp.com/send?phone=${_phone}`;
+    const url = `https://api.whatsapp.com/send?phone=57${_phone}`;
     if(_phone ===  process.env.REACT_APP_ROUTE_QR_CODE) navigate("/code");
     else window.open(url, '_blank');
   }
