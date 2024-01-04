a42d495dd8e549e3511c5ad93ed07cbc143d5a1f
alexgm5555 <alexgm5555@hotmail.com>
2024-01-04 12:13:38 -0500

    fix

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -9,12 +9,12 @@ export const Main:FC = () => {
   let navigate = useNavigate();
 
   const mainSuccess = (_phone: string) => {
-    if (process.env.REACT_APP_ENVIROMENT === 'devs') console.log('enviado');
-    else {
-      const url = `https://api.whatsapp.com/send?phone=57${_phone}`;
-      if(_phone ===  process.env.REACT_APP_ROUTE_QR_CODE) navigate("/code");
-      else window.open(url, '_blank');
-    }
+    // if (process.env.REACT_APP_ENVIROMENT === 'devs') console.log('enviado');
+    // else {
+    const url = `https://api.whatsapp.com/send?phone=57${_phone}`;
+    if(_phone ===  process.env.REACT_APP_ROUTE_QR_CODE) navigate("/code");
+    else window.open(url, '_blank');
+    // }
   }
 
   return (
